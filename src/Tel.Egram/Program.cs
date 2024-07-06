c38ff9a41584160b314936b72faa5db3682f1d0f
morningsage <morningsage@none.com>
2024-07-06 11:24:27 -0400

    Move service collection to BuildAvaloniaApp so that it'll run in design mode and rework Program class

--- src/Tel.Egram/Program.cs
+++ src/Tel.Egram/Program.cs
@@ -6,86 +6,41 @@ using Tel.Egram.Application;
 
 namespace Tel.Egram;
 
-public class Program
+internal abstract class Program
 {
-        
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
-        CollectServices();
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime([]);
-
-        // Run(Locator.Current);
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
-    public static void CollectServices()
+    private static void CollectServices(IMutableDependencyResolver services)
     {
-        var resolver = Locator.CurrentMutable;
+        services.AddUtils();
+        services.AddTdLib();
+        services.AddPersistance();
+        services.AddServices();
             
-        resolver.AddUtils();
-        resolver.AddTdLib();
-        resolver.AddPersistance();
-        resolver.AddServices();
-            
-        resolver.AddComponents();
-        resolver.AddApplication();
-        resolver.AddAuthentication();
-        resolver.AddWorkspace();
-        resolver.AddSettings();
-        resolver.AddMessenger();
+        services.AddComponents();
+        services.AddApplication();
+        services.AddAuthentication();
+        services.AddWorkspace();
+        services.AddSettings();
+        services.AddMessenger();
     }
         
-    public static AppBuilder BuildAvaloniaApp() =>
-        AppBuilder.Configure<MainApplication>()
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        // As BuildAvaloniaApp is used by the designer, we collect services here so that things will be loaded correctly
+        CollectServices(Locator.CurrentMutable);
+        
+        return AppBuilder.Configure<MainApplication>()
             .UsePlatformDetect()
             .WithInterFont()
             .UseReactiveUI()
-            .UseSkia()
             .LogToTrace();
-        
-    private static void Run(
-        IReadonlyDependencyResolver resolver)
-    {
-        //var app = resolver.GetService<MainApplication>();
-        //var builder = AppBuilder.Configure(app);
-        //var runtime = builder.RuntimePlatform.GetRuntimeInfo();
-        //var model = new MainWindowModel();
-        //
-        //switch (runtime.OperatingSystem)
-        //{
-        //    case OperatingSystemType.OSX:
-        //        builder.UseAvaloniaNative()
-        //            .With(new AvaloniaNativePlatformOptions
-        //            {
-        //                UseGpu = true,
-        //                UseDeferredRendering = true
-        //            })
-        //            .UseSkia();
-        //        break;
-        //    
-        //    case OperatingSystemType.Linux:
-        //        builder.UseX11()
-        //            .With(new X11PlatformOptions
-        //            {
-        //                UseGpu = true
-        //            })
-        //            .UseSkia();
-        //        break;
-        //    
-        //    default:
-        //        builder.UseWin32()
-        //            .With(new Win32PlatformOptions
-        //            {
-        //                UseDeferredRendering = true
-        //            })
-        //            .UseSkia();
-        //        break;
-        //}
-//
-        //builder.UseReactiveUI();
-
-        //model.Activator.Activate();
-        //builder.Start<MainWindow>(() => model);
-        //model.Activator.Deactivate();
     }
 }
